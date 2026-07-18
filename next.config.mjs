bc5e2fb8b084beb0dda1863ce8125004d4a8d050
JaimeEstuardo <jaime.e.florian@gmail.com>
2026-07-18 11:09:01 -0600

    FIX: Configuración de servidor y rutas oficial

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Ignora errores de linter para permitir el despliegue
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Ignora errores de tipos para permitir el despliegue
     ignoreBuildErrors: true,
   },
-  // Forzamos el uso de nodejs estándar
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client"],
-  }
+  // Nombre corregido según los logs de Vercel
+  serverExternalPackages: ["@prisma/client"],
 };
 
 export default nextConfig;
